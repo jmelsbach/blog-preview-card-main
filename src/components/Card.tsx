1e454392e2b734e71a891129f0def77b4ad90714
Johannes Melsbach <johannes@melsbach.dev>
2024-06-24 09:58:51 +0200

    chore: Refactor Card component for improved styling and structure

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,12 +18,14 @@ function Card({
   authorImage,
 }: CardProps) {
   return (
-    <div className="max-w-96">
-      <img src={blogImage}></img>
-      <div className="tag-row">{categories}</div>
-      <div>Published {date}</div>
-      <h1>{title}</h1>
-      <p>{description}</p>
+    <div className="border-2 max-w-96 p-6 rounded-[20px]">
+        <img src={blogImage} className="rounded-[20px] w-full"></img>
+        <div className="my-6">
+        <div className="my-3">{categories}</div>
+        <div className="my-3">Published {date}</div>
+        <h1 className="my-3">{title}</h1>
+        <p>{description}</p>
+      </div>
       <div>
         <img src={authorImage} alt="Author Image" />
         <span>{authorName}</span>
