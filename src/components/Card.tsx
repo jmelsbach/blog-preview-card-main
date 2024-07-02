b0adf87fc58bc7b7a5c16748b441daed1c4bface
Johannes Melsbach <johannes@melsbach.dev>
2024-07-02 17:35:45 +0000

    chore: Refactor Card component for improved styling and structure

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,8 +20,8 @@ function Card({
   authorImage,
 }: CardProps) {
   return (
-    <div className="font-custom bg-white border-black max-w-[327px] sm:max-w-[384px] p-6 rounded-[20px] shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] hover:shadow-[16px_16px_0px_0px_rgba(0,0,0,1)] flex-col">
-      <img src={blogImage} className="rounded-[20px] w-full h-50 object-center"></img>
+    <div className="font-custom bg-white border border-black max-w-[327px] sm:max-w-[384px] p-6 rounded-[20px] shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] hover:shadow-[16px_16px_0px_0px_rgba(0,0,0,1)] flex-col">
+      <img src={blogImage} alt="article illustration" className="rounded-[10px] w-full h-50 object-center"></img>
       <div className="my-6">
         <Tag name={categories[0]}></Tag>
         <div className="my-3 text-xs sm:text-sm">Published {date}</div>
@@ -30,7 +30,7 @@ function Card({
         </h1>
         <p className="text-sm sm:text-base text-[#7F7F7F]">{description}</p>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-3">
         <img src={authorImage} className="w-8" alt="Author Image" />
         <span className="text-center font-extrabold text-xs sm:text-sm">{authorName}</span>
       </div>
